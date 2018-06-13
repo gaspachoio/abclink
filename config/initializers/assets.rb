138230c37a4b8349cce7fa6136a85051fe0df559
Manuel Ortega <manuel@lasindias.coop>
2018-06-13 12:07:03 -0500

    Updated precompile assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-  Rails.application.config.assets.precompile += %w( devise.css bootstrap_application.css bootstrap_application.js)
+  Rails.application.config.assets.precompile += %w( devise.css bootstrap_application.css bootstrap_application.js abclink-logo-navbar.png abclink-logo.png)
